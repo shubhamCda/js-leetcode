d391c26425b9224a3a6a785e7f8cc20025d513ec
shubhamCda <shubhambodalkar281@gmail.com>
2025-08-24 20:58:38 +0530

    Add isPowerFour optimized function

--- powerOfFour.js
+++ powerOfFour.js
@@ -1,17 +1,17 @@
-const isPowerOfFour = function (n) {
-	const x = Math.sqrt(n);
-	console.log(x);
-	if (x == 1) {
-		return true;
-	}
-	if (n > 0) {
-		let cal = Math.sqrt(x) % 2;
-		if (cal == 0) {
-			return true;
-		}
-	}
-	return false;
-};
+// const isPowerOfFour = function (n) {
+// 	const x = Math.sqrt(n);
+// 	console.log(x);
+// 	if (x == 1) {
+// 		return true;
+// 	}
+// 	if (n > 1) {
+// 		let cal = Math.sqrt(x) % 2;
+// 		if (cal == 0) {
+// 			return true;
+// 		}
+// 	}
+// 	return false;
+// };
 
 console.log(isPowerOfFour(-2147483648));
 console.log(isPowerOfFour(8));
@@ -19,3 +19,30 @@ console.log(isPowerOfFour(4));
 console.log(isPowerOfFour(36));
 
 // console.log(Math.sqrt(16));
+
+function isPowerOfFour(n) {
+	if (n < 1) {
+		return false;
+	}
+
+	while (n % 4 === 0) {
+		n /= 4;
+	}
+	return n===1;
+}
+
+
+// function isPowerOfFour(n) {
+// 	if (n < 1) {
+// 		return false;
+// 	}
+
+// 	while (n>1) {
+// 		if (n % 4 !== 0) {
+// 			return false
+// 		}
+
+// 		n = n / 4;
+// 	}
+// 	return true;
+// }
