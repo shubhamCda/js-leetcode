192aac3f09d99933dcce0bd453d363b38d94aad1
shubhamCda <shubhambodalkar281@gmail.com>
2025-09-22 18:46:44 +0530

    Add DP grid traveller example

---
+++ DSA/DSA-patterns/DP/gridTraveller.js
@@ -0,0 +1,18 @@
+const gridTraveller = (m,n, memo={}) => {
+  const key = m + ',' + n;
+  if (key in memo) return memo[key];
+  if (m === 1 && n === 1) return n;
+  if (m === 0 || n === 0) return 0;
+
+  memo[key] = gridTraveller(m - 1, n, memo) + gridTraveller(m, n - 1, memo);
+  
+  console.log("memo", memo);
+  
+  return memo[key];
+
+}
+
+console.log(gridTraveller(1,1));
+console.log(gridTraveller(2,3));
+console.log(gridTraveller(3,2));
+// console.log(gridTraveller(18,18));
